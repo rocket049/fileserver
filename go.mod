7ce05de09a73c2046b3dbff56ad5795cfebfac3c
rocket049 <fuhuizn@163.com>
2020-06-22 12:07:16 +0800

    .

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module fileserver
 go 1.12
 
 require (
+	gitee.com/rocket049/discover-go v0.0.0-20200622001129-c94ccea7cd7e
 	github.com/skip2/go-qrcode v0.0.0-20190110000554-dc11ecdae0a9
 	github.com/skratchdot/open-golang v0.0.0-20190402232053-79abb63cd66e
 )
--- main.go
+++ main.go
@@ -7,13 +7,46 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
 	"strings"
+	"time"
 
+	"gitee.com/rocket049/discover-go"
 	"github.com/skratchdot/open-golang/open"
 
 	qrcode "github.com/skip2/go-qrcode"
 )
+
+func runDiscover() *discover.DiscoverClient {
+	client := discover.NewClient()
+	ok := client.Append("http", 6868, "index", "FileServer", "share files")
+	if ok == false {
+		server := discover.NewServer()
+		go server.Serve(false)
+		time.Sleep(time.Millisecond * 100)
+		client.Append("http", 6868, "index", "FileServer", "share files")
+	}
+	res := client.Query()
+	fmt.Println("Servers:")
+	for _, v := range res {
+		if v.Name == "FileServer" {
+			fmt.Println(v.Href, v.Name, v.Title)
+		}
+	}
+	return client
+}
+
+func listServers() {
+	client := discover.NewClient()
+	res := client.Query()
+	fmt.Println("Servers:")
+	for _, v := range res {
+		if v.Name == "FileServer" {
+			fmt.Println(v.Href, v.Name, v.Title)
+		}
+	}
+}
 
 func showAddr() {
 	ifs, err := net.Interfaces()
@@ -66,7 +99,12 @@ func showPng(fn, title string) {
 func main() {
 	var share = flag.String("share", ".", "Share files in this DIR")
 	var upload = flag.String("upload", ".", "Upload files to this DIR")
+	var browse = flag.Bool("browse", false, "List server in this lan.")
 	flag.Parse()
+	if *browse {
+		listServers()
+		return
+	}
 	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte(mainPage))
@@ -74,5 +112,13 @@ func main() {
 	setShareDir(*share)
 	setUploadDir(*upload)
 	showAddr()
-	http.ListenAndServe(":6868", nil)
+
+	c := runDiscover()
+	defer c.Remove("http", 6868, "index")
+
+	go http.ListenAndServe(":6868", nil)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	fmt.Println("Exit")
 }
